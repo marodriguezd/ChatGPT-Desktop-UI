63a102d3fbddcf9fdc5e49b0a99951caea53b6d3
marodriguezd <migueadali@gmail.com>
2023-11-17 18:38:09 +0100

    Ahora tiene memoria

--- main.py
+++ main.py
@@ -5,16 +5,31 @@ import openai
 class ChatGPTClient:
     def __init__(self, api_key):
         self.api_key = api_key
+        self.message_history = []
         openai.api_key = self.api_key
 
     def get_engines(self):
         return openai.Engine.list()
 
+    def add_message_to_history(self, role, content):
+        self.message_history.append({
+            "role": role,
+            "content": content
+        })
+
     def create_chat(self, engine, prompt):
+        self.add_message_to_history("user", prompt)
+
         try:
-            response = openai.ChatCompletion.create(model=engine, messages=[{"role": "user", "content": prompt}])
+            response = openai.ChatCompletion.create(
+                model=engine,
+                messages=self.message_history
+            )
             # response = openai.Completion.create(engine=engine, prompt=prompt)
-            return response['choices'][0]['message']['content'].strip()
+            chat_response = response['choices'][0]['message']['content'].strip()
+            self.add_message_to_history("assistant", chat_response)
+            return chat_response
+            # return response['choices'][0]['message']['content'].strip()
             # return response.choices[0].text.strip()
         except Exception as e:
             print(f"An error occurred: {e}")
